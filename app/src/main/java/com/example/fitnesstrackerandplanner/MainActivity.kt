232bb809d3e26df5a8d7ec2956eac8e26559bff9
Avassaa <tutuncubaha@gmail.com>
2024-05-10 19:02:16 +0300

    Test

--- app/src/main/java/com/example/fitnesstrackerandplanner/MainActivity.kt
+++ app/src/main/java/com/example/fitnesstrackerandplanner/MainActivity.kt
@@ -39,6 +39,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     FitnessTrackerAndPlannerTheme {
-        Greeting("Android")
+        Greeting("Test")
     }
 }
