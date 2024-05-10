d24dbe01d2dd08e85374b21d4e21a35afbdea93f
irem karakaplan <irem@irem-MacBook-Air.local>
2024-05-10 18:26:43 +0300

    Initial commit

--- app/src/main/java/com/example/fitnesstrackerandplanner/MainActivity.kt
+++ app/src/main/java/com/example/fitnesstrackerandplanner/MainActivity.kt
@@ -18,10 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             FitnessTrackerAndPlannerTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
                 }
             }
