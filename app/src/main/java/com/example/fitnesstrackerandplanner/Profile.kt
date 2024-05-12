3380f11247340540e494d9124d0638dcb8e6d400
Avassaa <tutuncubaha@gmail.com>
2024-05-12 17:12:35 +0300

    Changed bottom navigation bar

--- app/src/main/java/com/example/fitnesstrackerandplanner/Profile.kt
+++ app/src/main/java/com/example/fitnesstrackerandplanner/Profile.kt
@@ -1,16 +1,23 @@
 package com.example.fitnesstrackerandplanner
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.fitnesstrackerandplanner.ui.theme.PurpleGrey40
 
@@ -34,15 +41,45 @@ fun Profile() {
 */
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .align(Alignment.Center),
+                .fillMaxSize(),
             horizontalAlignment =  Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+
         ){
+            //aynı satır içinde fotoğraf+yazı
+
+            Row() {
+                //Image()
+                Text(
+                    text = "Set weight",
+                    fontSize = 30.sp,
+                    color = Color.Magenta,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { },
+
+                    )
+            //aynı satır içinde fotoğraf+yazı
+            }
+            Row() {
+                //Image()
+                Text(
+                    text = "Set height",
+                    fontSize = 30.sp,
+                    color = Color.Magenta,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable { }
+                )
+            }
+
             Text(
-                text ="Profile page",
+                text ="<--------------->",
                 fontSize = 30.sp,
-                color = Color.Magenta
+                color = Color.Magenta,
+                modifier=Modifier
+                    .fillMaxWidth()
+                    .clickable {  }
             )
 
         }
